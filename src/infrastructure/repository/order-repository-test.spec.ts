97c2a611ad2c7d9e9f66d9ad340740d370846d8c
tsvetkov <atsvetkov2020@yandex.ru>
2026-05-30 16:03:07 +0300

    OrderRepository

---
+++ src/infrastructure/repository/order-repository-test.spec.ts
@@ -0,0 +1,34 @@
+import { OrderRepository } from './order-repository';
+import { Order } from '@/domain/aggregateroots/order';
+import { OrderLine } from '@/domain/entities/order-line';
+import { Product } from '@/domain/entities/product';
+import { OrderFactory } from '@/domain/factories/order-factory';
+import { Money } from '@/domain/vo/money';
+
+describe('OrderRepository комплексный тест создаем-сохраняем-извлекаем-проверяем заказ', () => {
+    it('Тест создаем-сохраняем в репозиторий-извлекаем-проверяем заказ', async () => {
+        const orderId = 'order-001';
+        const repository = new OrderRepository();
+        const validProduct = new Product('prod-001', 'Тестовый продукт', new Money(1000, 'RUB'));
+        const validProduct2 = new Product('prod-002', 'Второй продукт', new Money(500, 'RUB'));
+        const items = [
+            { product: validProduct, quantity: 2 }, 
+            { product: validProduct2, quantity: 3 }
+        ];
+        const order: Order = OrderFactory.createWithItems(items, orderId);
+        repository.save(order);
+        const retrieved = await repository.getById(orderId);
+
+        const retrievedOrder: Order = retrieved as Order;
+        expect(retrievedOrder).toBeDefined();
+        expect(retrievedOrder.getId()).toBe(orderId)
+        expect(retrievedOrder.getItems()).toHaveLength(2);
+        retrievedOrder.getItems().forEach((line: OrderLine, index: number) => {
+          expect(line.getQuantity()).toBe(items[index].quantity);
+          expect(line.getProduct()).toBe(items[index].product);
+        });
+        expect(retrievedOrder.getTotal()).toEqual(new Money(3500, 'RUB'));
+
+
+    });
+});
---
+++ src/infrastructure/repository/order-repository.ts
@@ -0,0 +1,24 @@
+import { Order } from "@/domain/aggregateroots/order";
+import { IOrderRepository } from "@/domain/repository/iorder-repository";
+
+export class OrderRepository implements IOrderRepository {
+    private orders: Map<string, Order> = new Map();
+     
+    async getById(id: string): Promise<Order | null> {
+        const product = this.orders.get(id);
+        return product || null;
+    }
+
+    async getAll(): Promise<Order[]> {
+        return Array.from(this.orders.values());
+    }
+
+    async save(order: Order): Promise<void> {
+        this.orders.set(order.getId(), order);
+    }
+
+    async delete(id: string): Promise<void> {
+        this.orders.delete(id);
+    }
+}
+    
